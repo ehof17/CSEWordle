8229439ac3dd1cf48430d33912c9b6f86c3f963a
alexalvarado03 <aalva113@asu.edu>
2025-04-13 19:29:26 -0700

    added generateWord func to the wordle logic

--- WordleLogic/Logic.cs
+++ WordleLogic/Logic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,21 @@ namespace WordleLogic
 {
     public static class Logic
     {
-        public static string generateWord()
+        public static string GenerateWord(string filePath)
         {
-            return "newWord";
+            if(!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("The file could not be found.\n", filePath);
+            }
+            string[] words = File.ReadAllLines(filePath);
+
+            if(words.Length == 0)
+            {
+                throw new InvalidOperationException("The file is empty.\n");
+            }
+
+            Random rand = new Random();
+            return words[rand.Next(words.Length)].Trim();
         }
     }
 }
